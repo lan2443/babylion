aa0e23b89a5287b3adb9cf183d757d933f32e096
lan2443 <lan2443@naver.com>
2023-06-27 19:37:29 +0900

    third commit

---
+++ 01.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}", // 적용할 js파일 경로 지정
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
